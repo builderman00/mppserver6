7ae3ae77b7365d66b640b153b99ad572922e8356
Builderman <yhi958232@gmail.com>
2024-10-15 18:37:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 if (config.uws) {
-module.exports = require('uwebsockets').App({});
-} else module.exports = require('http').createServer(app);
+    module.exports = require('http').createServer(app);
+}
